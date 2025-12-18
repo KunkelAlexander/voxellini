87feba570bc6784c0c22c1bc0bf5db6a18dd06ba
Alexander Kunkel <kunkel.alexander@protonmail.com>
2025-12-18 10:08:48 +0100

    First commit with working FPV scene

---
+++ player.gd
@@ -0,0 +1,45 @@
+extends CharacterBody3D
+
+@export var speed := 8.0
+@export var mouse_sensitivity := 0.002
+@export var vertical_speed := 6.0
+
+var pitch := 0.0
+
+@onready var camera := $Camera3D
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+
+		pitch -= event.relative.y * mouse_sensitivity
+		pitch = clamp(pitch, deg_to_rad(-89), deg_to_rad(89))
+		camera.rotation.x = pitch
+
+	if event.is_action_pressed("exit"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _physics_process(delta):
+	var direction := Vector3.ZERO
+
+	if Input.is_action_pressed("move_forward"):
+		direction -= transform.basis.z
+	if Input.is_action_pressed("move_back"):
+		direction += transform.basis.z
+	if Input.is_action_pressed("move_left"):
+		direction -= transform.basis.x
+	if Input.is_action_pressed("move_right"):
+		direction += transform.basis.x
+
+	if Input.is_action_pressed("move_up"):
+		direction += transform.basis.y
+	if Input.is_action_pressed("move_down"):
+		direction -= transform.basis.y
+
+	direction = direction.normalized()
+
+	velocity = direction * speed
+	move_and_slide()
